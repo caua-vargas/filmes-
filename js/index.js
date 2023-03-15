c065dd508dbd813c69fbd1c05eb8c98abe77475d
caua vargas <caua-rvargas@educar.rs.gov.br>
2023-03-15 13:35:07 -0300

    terceiro commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+let inputBuscarFilme = document.querySelector("#input-buscar-filme");
+let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
+
+    btnBuscarFilme.onclick.preventDefault() = async() =>{
+       
+    if(inputBuscarFilme.value.length>0){
+    let filmes = new Array();
+    fetch("http://www.omdbapi.com/?apikey=f9f61240&s="+inputBuscarFilme.value)
+    .then((resp)=> resp.json())
+    .then((resp)=>{ console.log(resp)
+    resp.Search.forEach((item)=>{
+    console.log(item);
+    let filme=new Filme(
+    item.imdbID,
+    item.Title,
+    item.Year,
+    null,
+    null,
+    item.Poster,
+    null,
+    null,
+    null,
+    null,
+    null
+    );
+    filmes.push(filme);
+});
+listarFilmes(filmes);
+    })
+
+    }
+return false;   
+}
+
+let listarFilmes = async (filmes) =>{
+    let listaFilmes = await document.querySelector("#lista-filmes");
+    listaFilmes.innerHTML = "";
+    console.log(listaFilmes);
+    if (filmes.length > 0) {
+    filmes.forEach(async(filme)=> {
+    listaFilmes.appendChild(await filme.getCard());
+    });
+}
+}
+
+
